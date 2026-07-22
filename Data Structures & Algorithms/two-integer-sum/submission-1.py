3390cdc574681f04fdf1ec9091f6e75eef18228c
c7Ventura <157093226+c7Ventura@users.noreply.github.com>
2026-07-22 08:17:48 -0700

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    #TODO: implement as Hashmap 2 pass
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+     for i in range(len(nums)): 
+        # second pointer pointing at end of list
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] == target:
+                    return [i, j]
+                # cant add an item at same index. No other case.
+     return [-1, -1]
+            
